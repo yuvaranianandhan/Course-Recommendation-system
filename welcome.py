bb194f9eda54969459abc7ebe21c3ff2a0746001
Yuvarani Anandhan <111055786+yuvaranianandhan@users.noreply.github.com>
2024-02-21 23:50:33 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,34 +1,19 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_extras.let_it_rain import rain
-from PIL import Image
 
-  
-  
-st.set_page_config(page_title="CRS",page_icon = "🫰")
+# Setting page config
+st.set_page_config(page_title="CRS", page_icon="🫰")
 
-
-
-
-
+# Title and subtitle
 st.markdown("<h1 style='text-align: Left ; color: white;'>  COURSE RECOMMENDATION SYSTEM </h1>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center ; color: violet;'> Discover a plethora of courses at your fingertips!</h4>", unsafe_allow_html=True)
 
-  
-#To load image
-img = Image.open(r'C:\Users\Yuva\.vscode\Streamlit dev\images.png')
-image_bytes = img.resize((700,400))
+# Load image
+img_path = "path/to/your/image.png"  # Provide the correct path here
+st.image(img_path, width=700, caption="Image Caption")
 
-
-co_1, co_2, co_3 = st.columns(3)
-with co_1:
-    st.image(image_bytes, use_column_width=False)
-
-
-  
-  
 # Raining Emoji
-  
 rain(
     emoji="🕮",
     font_size=50,  # the size of emoji
@@ -36,13 +21,7 @@ rain(
     animation_length="infinite",  # for how much time the animation will happen
 )
 
-
-#sc = st.sidebar.columns(2)
-#sc[0].image(r'C:\Users\Yuva\.vscode\Streamlit dev\images.png', width=150)
-#sc[1].write("  ")
-
-
-
+# Subheader and button
 st.subheader(":blue[Discover your perfect course - click here to explore! :point_down:]")
 want_to_contribute = st.button("click here")
 if want_to_contribute:
